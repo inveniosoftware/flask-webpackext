e4bfa226a104f6991f27253e121be7e6c1835949
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-09-14 22:32:18 +0200

    tests: modernize run-tests.sh

--- run-tests.sh
+++ run-tests.sh
@@ -2,13 +2,18 @@
 #
 # This file is part of Flask-WebpackExt
 # Copyright (C) 2017 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Flask-WebpackExt is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
 # more details.
 
-pydocstyle flask_webpackext tests docs && \
-isort -rc -c -df && \
-check-manifest --ignore ".travis-*" && \
-sphinx-build -qnNW docs docs/_build/html && \
-python setup.py test
+# Quit on errors
+set -o errexit
+
+# Quit on unbound symbols
+set -o nounset
+
+python -m check_manifest
+python -m sphinx.cmd.build -qnNW docs docs/_build/html
+python -m pytest
