a47ac88caf67a7e1ac1285145d604ef51e95bf83
Christoph Ladurner <christoph.ladurner@tugraz.at>
2024-09-14 22:19:30 +0200

    migrate: setup.py to setup.cfg

--- setup.py
+++ setup.py
@@ -2,6 +2,7 @@
 #
 # This file is part of Flask-WebpackExt
 # Copyright (C) 2017 CERN.
+# Copyright (C) 2024 Graz University of Technology.
 #
 # Flask-WebpackExt is free software; you can redistribute it and/or modify
 # it under the terms of the Revised BSD License; see LICENSE file for
@@ -9,88 +10,6 @@
 
 """Webpack integration for Flask."""
 
-import os
+from setuptools import setup
 
-from setuptools import find_packages, setup
-
-readme = open('README.rst').read()
-history = open('CHANGES.rst').read()
-
-tests_require = [
-    'check-manifest>=0.25',
-    'coverage>=4.0',
-    'isort>=4.3.21',
-    'pydocstyle>=1.0.0',
-    'pytest-cache>=1.0',
-    'pytest-cov>=1.8.0',
-    'pytest-pep8>=1.0.6',
-    'pytest>=2.8.0',
-]
-
-extras_require = {
-    'docs': [
-        'Sphinx>=1.5.1,<3',
-        'sphinx-click>=1.4.0',
-    ],
-    'tests': tests_require,
-}
-
-extras_require['all'] = []
-for reqs in extras_require.values():
-    extras_require['all'].extend(reqs)
-
-setup_requires = [
-    'pytest-runner>=2.6.2',
-]
-
-install_requires = [
-    'Flask>=0.11',
-    'pynpm>=0.1.1',
-    'pywebpack>=1.0.0',
-]
-
-packages = find_packages()
-
-
-# Get the version string. Cannot be done with import!
-g = {}
-with open(os.path.join('flask_webpackext', 'version.py'), 'rt') as fp:
-    exec(fp.read(), g)
-    version = g['__version__']
-
-setup(
-    name='flask-webpackext',
-    version=version,
-    description=__doc__,
-    long_description=readme + '\n\n' + history,
-    keywords='flask webpack',
-    license='BSD',
-    author='CERN',
-    author_email='info@inveniosoftware.org',
-    url='https://github.com/inveniosoftware/flask-webpackext',
-    packages=packages,
-    zip_safe=False,
-    include_package_data=True,
-    extras_require=extras_require,
-    install_requires=install_requires,
-    setup_requires=setup_requires,
-    tests_require=tests_require,
-    entry_points={
-        'flask.commands': [
-            'webpack = flask_webpackext.cli:webpack',
-        ],
-    },
-    classifiers=[
-        'Environment :: Web Environment',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Libraries :: Python Modules',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-        'Development Status :: 5 - Production/Stable',
-    ],
-)
+setup()
